79d9e838896be637028bf21d0a84726bc51c54a9
Jędrzej Antkiewicz <jedrzej.antkiewicz.sp@lhsystems.com>
2023-07-04 14:49:03 +0200

    Added base domain models and main process

---
+++ src/main/java/com/example/cinemaapi/tickets/domain/Reservations.java
@@ -0,0 +1,15 @@
+package com.example.cinemaapi.tickets.domain;
+
+import java.util.UUID;
+
+public class Reservations {
+
+    public UUID createReservation(final ReservationRequest request) {
+        //validate reservation request
+        return null;
+    }
+
+    public Ticket confirmReservation(UUID reservationId) {
+        return null;
+    }
+}
