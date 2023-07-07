ce701001f0f1ead08c3fdfa9baf420704dc99970
Jędrzej Antkiewicz <jedrzej.antkiewicz.sp@lhsystems.com>
2023-07-07 14:03:15 +0200

    Moved enums to separate classes

--- src/main/java/com/example/cinemaapi/tickets/domain/Seat.java
+++ src/main/java/com/example/cinemaapi/tickets/domain/Seat.java
@@ -3,10 +3,3 @@ package com.example.cinemaapi.tickets.domain;
 public record Seat(int seatNumber, int row, SeatStatus status, SeatType isVIP) {
 }
 
-public enum SeatStatus {
-    UNOCCUPIED, OCCUPIED
-}
-
-public enum SeatType {
-    REGULAR, VIP
-}
---
+++ src/main/java/com/example/cinemaapi/tickets/domain/SeatStatus.java
@@ -0,0 +1,5 @@
+package com.example.cinemaapi.tickets.domain;
+
+public enum SeatStatus {
+    UNOCCUPIED, OCCUPIED
+}
